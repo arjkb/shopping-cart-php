4fcf5a83b052cdf33cc827af0c892d69eea29178
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2019-07-20 15:50:43 +0530

    display products from the database, along with buttons to add them to cart of buy them

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
-  echo "php works!";
+  require_once "pdo.php";
+  $stmt_select = $pdo->query('SELECT id, pname, unitprice FROM product');
 ?>
-
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
@@ -11,5 +11,32 @@
   <body>
     <h1>Product List</h1>
 
+    <table>
+      <thead>
+        <th>Product Name</th>
+        <th>Unit Price</th>
+        <th>Action</th>
+      </thead>
+      <tbody>
+      <?php while ($row = $stmt_select->fetch(PDO::FETCH_ASSOC)): ?>
+        <tr>
+          <td><?= $row['pname'] ?></td>
+          <td><?= $row['unitprice'] ?></td>
+          <td>
+            <form class="" action="" method="POST">
+              <input type="hidden" name="addtocart_id" value="<?= $row['id'] ?>">
+              <button type="submit">Add to cart</button>
+            </form>
+          </td>
+          <td>
+            <form class="" action="" method="POST">
+              <input type="hidden" name="buynow_id" value="<?= $row['id'] ?>">
+              <button type="submit">Buy now</button>
+            </form>
+          </td>
+        </tr>
+      <?php endwhile; ?>
+      </tbody>
+    </table>
   </body>
 </html>
