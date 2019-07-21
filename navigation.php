5444410580302c733cd636f251dfddd3f763d692
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2019-07-21 22:56:35 +0530

    make navigation bar look decent using bootstrap

--- navigation.php
+++ navigation.php
@@ -1,13 +1,19 @@
-<nav>
-  <ul>
-    <li><a href="index.php">Home</a></li>
-    <li><a href="#">About</a></li>
-    <?php if(isset($_SESSION['username']) || isset($_SESSION['admin'])): ?>
-      <li><a href="logout.php">Logout</a></li>
-    <?php else: ?>
-      <li><a href="login.php">Login</a></li>
-    <?php endif; ?>
-    <li><a href="signup.php">Sign Up</a></li>
-    <li><a href="admin.php">Admin Login</a></li>
-  </ul>
+<nav class="navbar navbar-expand-lg navbar-light bd-light">
+  <a class="navbar-brand" href="#">Navbar Brand</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle Navigation" name="button">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
+      <li class="nav-item"><a class="nav-link" href="#">About</a></li>
+      <?php if(isset($_SESSION['username']) || isset($_SESSION['admin'])): ?>
+        <li class="nav-item"><a class="nav-link" href="logout.php">Logout</a></li>
+      <?php else: ?>
+        <li class="nav-item"><a class="nav-link" href="login.php">Login</a></li>
+      <?php endif; ?>
+      <li class="nav-item"><a class="nav-link" href="signup.php">Sign Up</a></li>
+      <li class="nav-item"><a class="nav-link" href="admin.php">Admin Login</a></li>
+    </ul>
+  </div>
 </nav>
