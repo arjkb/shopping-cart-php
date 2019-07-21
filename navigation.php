26a4e84f81f1f279f55a41961b21b24a3e02258b
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2019-07-21 21:57:00 +0530

    show logout navigation link when admin is logged in

--- navigation.php
+++ navigation.php
@@ -2,7 +2,7 @@
   <ul>
     <li><a href="index.php">Home</a></li>
     <li><a href="#">About</a></li>
-    <?php if(isset($_SESSION['username'])): ?>
+    <?php if(isset($_SESSION['username']) || isset($_SESSION['admin'])): ?>
       <li><a href="logout.php">Logout</a></li>
     <?php else: ?>
       <li><a href="login.php">Login</a></li>
